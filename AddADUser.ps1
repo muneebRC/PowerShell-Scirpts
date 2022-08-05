90afbd213c0fd8d0a968e727228441fdd28442e1
muneebRC <109923415+muneebRC@users.noreply.github.com>
2022-08-05 12:11:53 -0400

    Update AddADUser.ps1

--- AddADUser.ps1
+++ AddADUser.ps1
@@ -1,13 +1,11 @@
-﻿Set-ExecutionPolicy Bypass
-Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope CurrentUser -Verbose
+Set-ExecutionPolicy -ExecutionPolicy Remotesigned -Scope CurrentUser -Verbose
 Import-Module ActiveDirectory
-Import-Module ActiveDirectory -Verbose
 Set-Location AD:
 New-ADUser -SamAccountName "firstL" -Name "first1 last3" -GivenName "first" -Surname "last" -DisplayName "First Last" -PasswordNeverExpires $true -UserPrincipalName "first.lasts@mssfulfillment.com" -Path "OU=S4LYN,OU=MSS,DC=mssfulfillment,DC=com" -AccountPassword(ConvertTo-SecureString "123summer" -AsPlainText -Force) -ScriptPath "login.bat" -Enabled $true -Verbose
 $refuser = get-aduser -Identity dianaA -Properties Memberof
 $refmemberof = $refuser.MemberOf
 $refmemberof | Add-ADGroupMember -Members firstL
 
-get-aduser -Identity dianaA -properties memberof | Select-Object -ExpandProperty memberof |  Add-ADGroupMember -Members firstL
+get-aduser -Identity tempuser -properties memberof | Select-Object -ExpandProperty memberof |  Add-ADGroupMember -Members firstL
 
-Set-ExecutionPolicy Restricted
+Set-ExecutionPolicy -ExecutionPolicy Restricted -Scope CurrentUser -Verbose
